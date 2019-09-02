44c1d81e6b34e572b3453c333fd47abb9d0e927e
Lucas Martines <lucas.alcantarilla97@gmail.com>
2019-09-01 21:21:21 -0300

    Criação das entidades!

---
+++ QuickBuy.Dominio/Entidades/Pedido.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace QuickBuy.Dominio.Entidades
+{
+    public class Pedido
+    {
+        public int Id { get; set; }
+        public DateTime DataPedido { get; set; }
+
+        public int UsuarioId { get; set; }
+
+        public DateTime DataPrevisaoEntrega { get; set; }
+
+        public string CEP { get; set; }
+
+        public string Estado { get; set; }
+
+        public string EnderecoCompleto { get; set; }
+
+        public string NumeroEndereco { get; set; }
+
+        public int FormaPagamentoId { get; set; }
+        public FormaPagamento FormaPagamento { get; set; }
+        /**
+         * Um pedido pode ter varios items de pedidos tipo
+         
+         * */
+        public ICollection<ItemPedido> ItensPedidos { get; set; }
+
+    }
+}
---
+++ QuickBuy.Dominio/ObjetoValor/FormaPagamento.cs
@@ -0,0 +1,45 @@
+﻿using QuickBuy.Dominio.ObjetoValor;
+using System;
+
+
+namespace QuickBuy.Dominio
+{
+    public class FormaPagamento
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+
+
+        public bool EhBoleto {
+            get
+            {
+                return Id == (int)TipoFormaPagamentoEnum.Boleto;
+            }
+        }
+        public bool EhCartao
+        {
+            get
+            {
+                return Id == (int)TipoFormaPagamentoEnum.CartaoCredito;
+            }
+        }
+        public bool EhDeposito
+        {
+            get
+            {
+                return Id == (int)TipoFormaPagamentoEnum.Deposito;
+            }
+        }
+        public bool NaoFoiDefinido
+        {
+            get
+            {
+                return Id == (int)TipoFormaPagamentoEnum.NaoDefinido;
+            }
+        }
+
+
+
+    }
+}
