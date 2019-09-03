52b6b995a3327b0ff6cda6386c4daafa7d158f17
Lucas Martines <lucas.alcantarilla97@gmail.com>
2019-09-02 21:04:44 -0300

    Modificaro as bibliotecas para .net standart, e adicionado o dbset

---
+++ QuickBuy.Dominio/Contratos/IBaseRepositorio.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace QuickBuy.Dominio.Contratos
+{
+    public interface IBaseRepositorio<TEntity>:IDisposable where TEntity:class
+    {
+        TEntity ObterPorId(int Id);
+        IEquatable<TEntity> ObterTodos();
+
+        void Atualizar(TEntity entity);
+        void Remover(TEntity entity);
+        void Adicionar(TEntity e);
+
+    }
+}
--- QuickBuy.Web/Startup.cs
+++ QuickBuy.Web/Startup.cs
@@ -5,17 +5,26 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.SpaServices.AngularCli;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using QuickBuy.Repositorio.Contexto;
 
 namespace QuickBuy.Web
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public IConfiguration Configuration { get; }
+        public Startup()//IConfiguration configuration
         {
-            Configuration = configuration;
+
+
+
+            var builder = new ConfigurationBuilder();
+            builder.AddJsonFile("config.json",optional:false ,reloadOnChange:true);
+
+            Configuration = builder.Build();
+
         }
 
-        public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
@@ -27,6 +36,12 @@ namespace QuickBuy.Web
             {
                 configuration.RootPath = "ClientApp/dist";
             });
+
+            var connectionString = Configuration.GetConnectionString("MysqlConnection");
+
+            services.AddDbContext<QuickBuyContexto>(options =>
+                                                             options.UseMySql(connectionString,
+                                                                       m => m.MigrationsAssembly("QuickBuy.Repositorio.Std")) );
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
